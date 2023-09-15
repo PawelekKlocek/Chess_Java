8802b2510b45caefd5c8b02e3fbbcacace683d55
Piotr <piotrkloc3k@gmail.com>
2023-09-15 11:02:45 +0200

    test

---
+++ src/Gra.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Gra extends JPanel implements KeyListener, ActionListener {
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("witam ja chłopa");
+
+        Gra gra=new Gra();
+        JFrame frame = new JFrame("Chess");
+
+        frame.setSize(900, 750);
+        // Pobranie rozmiaru ekranu
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        // Pobranie rozmiaru ramki
+        Dimension frameSize = frame.getSize();
+        int x = (screenSize.width - frameSize.width) / 2;
+        int y = (screenSize.height - frameSize.height) / 2;
+        frame.setLocation(x,y);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.add(gra);
+
+
+
     }
 }
